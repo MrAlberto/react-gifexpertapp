39067e585e4b391a79983501d761d927cbb6a166
Alberto Arribas <alberto@MacBook-Pro-de-Alberto.local>
2022-05-19 18:04:44 +0200

    first commit

---
+++ src/components/helpers/getGifs.js
@@ -0,0 +1,14 @@
+
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=tqB8Mnz0Mb9nJl780Qe1IsWL844HOogm`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifs;
+}
