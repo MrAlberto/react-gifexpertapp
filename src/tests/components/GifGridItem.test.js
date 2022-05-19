473266ab7b94b8c193d988b2e4706d476bcfbc8f
Alberto Arribas <alberto@MacBook-Pro-de-Alberto.local>
2022-05-19 19:51:40 +0200

    move helpers and hooks folders, and start tests

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { GifGridItem } from "../../components/GifGridItem";
+
+describe('Pruebas en <GifGridItem />',() =>{
+
+    const title = 'Un título';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>)
+
+    test('debe mostrar el componente correctamente', () =>{
+        
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    test('debe de tener un párrafo con el title',()=>{
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('debe tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+
+    test('debe de tener la clase animate__fadeIn', () => { 
+        const div = wrapper.find('div');
+        expect(div.prop('className')).toContain('animate__fadeIn');
+     })
+})
